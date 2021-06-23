0caa64c9fd09cf382ee6a618e20e0a7a5a8c4911
AzaT <39663655+ufaboy@users.noreply.github.com>
2021-06-23 18:32:18 +0500

    adding my files to my repository

---
+++ precache-manifest.40219db1eebab1160de6de39b67066dd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9c0ccd2624a475c6e93",
+    "url": "/library/css/app.1b5cbeed.css"
+  },
+  {
+    "revision": "c2d2c99fd86b5f08f5b566c31c681713",
+    "url": "/library/img/book-dead-solid.svg"
+  },
+  {
+    "revision": "a5ae5d5e358ece18439932c9111267c5",
+    "url": "/library/img/view_of_moon.jpg"
+  },
+  {
+    "revision": "282f867a6ddf24b9d6a084e69d643bc7",
+    "url": "/library/index.html"
+  },
+  {
+    "revision": "a9c0ccd2624a475c6e93",
+    "url": "/library/js/app.87c49022.js"
+  },
+  {
+    "revision": "c936fc1e77c3e9af73f8",
+    "url": "/library/js/chunk-2d0d6b32.0b839db5.js"
+  },
+  {
+    "revision": "770384f3556db6c2e836",
+    "url": "/library/js/chunk-vendors.49585d24.js"
+  },
+  {
+    "revision": "58ec51a1727894e7b0357a8c2cd71fcf",
+    "url": "/library/manifest.json"
+  },
+  {
+    "revision": "6c0c0b02c59a0e5b43917105fbeae507",
+    "url": "/library/robots.txt"
+  }
+]);
